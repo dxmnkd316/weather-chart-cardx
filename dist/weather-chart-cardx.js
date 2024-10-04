9a3229ea0d141e731d95b6df1ba08dea7603d022
dxmnkd316 <46753733+dxmnkd316@users.noreply.github.com>
2024-10-04 17:19:12 -0500

    Update weather-chart-cardx.js

--- dist/weather-chart-cardx.js
+++ dist/weather-chart-cardx.js
@@ -140,6 +140,7 @@ const locale = {
   en: {
     'tempHi': 'Temperature',
     'tempLo': 'Temperature night',
+	  'dewpoint': 'Dew Point',
     'precip': 'Precipitations',
     'feelsLike': 'Feels like',
     'units': {
