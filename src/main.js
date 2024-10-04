0f8a4e7bdb03ef32cfbfe3dbd9cd2a6c587a22cb
dxmnkd316 <46753733+dxmnkd316@users.noreply.github.com>
2024-10-04 17:43:44 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -457,8 +457,8 @@ drawChart({ config, language, weather, forecastItems } = this) {
     var d = forecast[i];
     dateTime.push(d.datetime);
     tempHigh.push(d.temperature);
-    if (showDewpoint) {
-      typeof d.dew_point !== 'undefined' ? (dewPoint.push(d.dew_point) : null);
+    if (showDewpoint && typeof d.dew_point !== 'undefined') {
+      dewPoint.push(d.dew_point);
     }
     if (typeof d.templow !== 'undefined') {
       tempLow.push(d.templow);
@@ -783,8 +783,8 @@ updateChart({ config, language, weather, forecastItems } = this) {
     var d = forecast[i];
     dateTime.push(d.datetime);
     tempHigh.push(d.temperature);
-    if (showDewpoint) {
-      typeof d.dew_point !== 'undefined' ? (dewPoint.push(d.dew_point) : null);
+    if (showDewpoint && typeof d.dew_point !== 'undefined') {
+      dewPoint.push(d.dew_point);
     }
     if (typeof d.templow !== 'undefined') {
       tempLow.push(d.templow);
